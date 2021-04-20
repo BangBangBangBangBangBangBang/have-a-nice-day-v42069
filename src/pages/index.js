167a38cf7dc059fb9da0fec641e0bbd50154ccd6
Aaron <cryptotrees@gmail.com>
2021-04-19 22:53:37 -0400

    Initial commit from gatsby: (https://github.com/chair28980/gatsby-starter-v3.git)

---
+++ src/pages/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '@global/layout/layout'
+import SEO from '@global/seo'
+
+const IndexPage = () => (
+	<Layout>
+		<SEO title='Home' />
+
+		<section className='wrapper-sm section text-block'>
+			<h2>Hello</h2>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam odio
+				minima magni, modi molestiae reiciendis illo? Totam cumque eum
+				temporibus!
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam odio
+				minima magni, modi molestiae reiciendis illo? Totam cumque eum
+				temporibus!
+			</p>
+			<Link to='/examples'>Examples Page</Link>
+		</section>
+	</Layout>
+)
+
+export default IndexPage
