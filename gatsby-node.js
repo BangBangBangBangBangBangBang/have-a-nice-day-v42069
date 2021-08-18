0ed9da5a70be64ff86920d087a253779ab1ae1b2
Tawnee <allprt89@gmail.com>
2021-08-18 11:33:49 -0600

    change node to async/await. rename seo component

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,38 +10,33 @@ exports.sourceNodes = async ({
 		headers: { 'X-API-KEY': process.env.API_KEY },
 	}
 
-	const hits = []
-
-	return fetch(
+	const results1 = await fetch(
 		'https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=50&collection=have-a-nice-day-tburd',
 		options
 	)
-		.then((response) => response.json())
-		.then((response) => {
-			hits.push(...response.assets)
-			return fetch(
-				'https://api.opensea.io/api/v1/assets?order_direction=desc&offset=50&limit=50&collection=have-a-nice-day-tburd',
-				options
-			)
+	const hits1 = await results1.json()
+
+	const results2 = await fetch(
+		'https://api.opensea.io/api/v1/assets?order_direction=desc&offset=50&limit=50&collection=have-a-nice-day-tburd',
+		options
+	)
+	const hits2 = await results2.json()
+
+	const hits = [...hits1.assets, ...hits2.assets]
+
+	return hits.map((hit) => {
+		createNode({
+			sold: hit.num_sales > 0,
+			name: hit.name,
+			url: hit.permalink,
+			// required fields
+			id: createNodeId(hit.id),
+			parent: null,
+			children: [],
+			internal: {
+				type: `Hits`,
+				contentDigest: createContentDigest(hit),
+			},
 		})
-		.then((response) => response.json())
-		.then((response) => hits.push(...response.assets))
-		.then(() => {
-			return hits.map((hit) => {
-				createNode({
-					sold: hit.num_sales > 0,
-					name: hit.name,
-					url: hit.permalink,
-					// required fields
-					id: createNodeId(hit.id),
-					parent: null,
-					children: [],
-					internal: {
-						type: `Hits`,
-						contentDigest: createContentDigest(hit),
-					},
-				})
-			})
-		})
-		.catch((err) => console.error(err))
+	})
 }
