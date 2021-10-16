3521d74838f572790fa5f9a4ab39070bfd6e77b7
Tawnee <allprt89@gmail.com>
2021-10-16 17:25:55 -0600

    add video component to header

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,6 +77,8 @@ module.exports = {
 				alias: {
 					'@components': path.resolve(__dirname, 'src/components'),
 					'@global': path.resolve(__dirname, 'src/components/global'),
+					'@assets': path.resolve(__dirname, 'src/assets'),
+					'@videos': path.resolve(__dirname, 'src/assets/videos'),
 					'@images': path.resolve(__dirname, 'src/assets/images'),
 					'@styles': path.resolve(__dirname, 'src/assets/styles/'),
 				},
--- src/components/global/header/header.js
+++ src/components/global/header/header.js
@@ -1,13 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StaticImage } from 'gatsby-plugin-image'
+import ReactPlayer from 'react-player/lazy'
+import videoFile from '@videos/lsdaopamp.mp4'
+import * as styles from './header.module.scss'
 
-const Header = () => (
-	<StaticImage
-		src='../../../assets/images/haveanicedaytrip1.jpeg'
-		alt='not a sheet of acid'
-		placeholder='blurred'
-		layout='fullWidth'
-	/>
-)
+const Header = () => {
+	const [reducedMotion, setReducedMotion] = useState(false)
+
+	useEffect(() => {
+		const QUERY = '(prefers-reduced-motion: no-preference)'
+		const mediaQueryList = window.matchMedia(QUERY)
+		const prefersReducedMotion = !mediaQueryList.matches
+		setReducedMotion(prefersReducedMotion)
+	}, [])
+
+	return (
+		<div className={styles.container}>
+			<StaticImage
+				src='../../../assets/images/haveanicedaytrip1.jpeg'
+				alt='not a sheet of acid'
+				placeholder='blurred'
+				layout='fullWidth'
+				className={styles.image}
+				style={{ position: 'absolute' }}
+			/>
+
+			{!reducedMotion && (
+				<ReactPlayer
+					url={videoFile}
+					width={'100%'}
+					height='500px'
+					volume={0}
+					controls
+					autoPlay={true}
+					muted={true}
+					playing
+					loop
+				/>
+			)}
+		</div>
+	)
+}
 
 export default Header
